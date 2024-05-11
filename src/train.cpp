614a4a2611d4feeb3122e0da4fabe94243b38e53
Kryazheva Valeria <150891739+vallerakr@users.noreply.github.com>
2024-05-11 19:50:46 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,61 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train() {
+    first = nullptr;
+    countOp = 0;
+}
+void Train::addCage(bool light) {
+    Cage* newCage = new Cage();
+    newCage->light = light;
+    if (!first) {
+        newCage->next = newCage;
+        newCage->prev = newCage;
+        first = newCage;
+    } else {
+        newCage->next = first;
+        newCage->prev = first->prev;
+        first->prev->next = newCage;
+        first->prev = newCage;
+    }
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
+
+int Train::getLength() {
+    Cage* currentWagon = first;
+    if (!currentWagon->light) {
+        currentWagon->light = true;
+    }
+    int wagonCount = 1;
+    int finalLength = 0;
+    bool keepCounting = true;
+    
+    while (keepCounting) {
+        currentWagon = currentWagon->next;
+        countOp += 1;
+        wagonCount += 1;
+
+        while (!currentWagon->light) {
+            currentWagon = currentWagon->next;
+            countOp += 1;
+            wagonCount += 1;
+        }
+
+        currentWagon->light = false;
+        finalLength = wagonCount;
+        
+        for (int tempCount = wagonCount; tempCount > 0; tempCount--) {
+            countOp += 1;
+            currentWagon = currentWagon->prev;
+        }
+
+        if (currentWagon->light == false) {
+            keepCounting = false;
+        }
+    }
+
+    return finalLength;
+}
